0490ee94724538cd189b93895ebe2d907ff6b283
Iacovos Kolokasis <kolokasis@ics.forth.gr>
2024-04-14 09:32:35 +0300

    [fix] Find docker process automatically

--- watch_live_updates.sh
+++ watch_live_updates.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-watch 'sudo docker exec -it ba6019f68937 /usr/jekyll/bin/bundle exec jekyll serve'
+DOCKER_ID=$(sudo docker ps | grep "modern-resume-theme" | awk '{print $1}')
+watch "sudo docker exec -it ${DOCKER_ID} /usr/jekyll/bin/bundle exec jekyll serve"
